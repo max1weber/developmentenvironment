511efc38bb2955c5b16561c8f375d41ed16f8fb4
Marc Weber <marc@integratiespecialist.net>
2024-01-20 11:14:16 +0100

    Add Script Chocolatey

---
+++ Scripts/Prepare-DevMachine.ps1
@@ -0,0 +1,10 @@
+﻿
+set-location  $PSScriptRoot 
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+
+$chocolateyAppList = "git,dotnet,l azure-cli,vscode,bicep, 7zip,choco jetbrainstoolbox,chocolatey-vscode,dotnet, docker-desktop,, azure-functions-core-tools , azurefunctions-vscode, azureaccount-vscode,  vscode-docker, vscode-markdown-all-in-one, vscode-tslint, vscode-csharpextensions, vscode-azurerm-tools, vscode-edge-debug"
+
+
+$dismAppList = "IIS-ASPNET45,IIS-CertProvider,IIS-ManagementService"
+
+Invoke-Expression ".\InstallDevMachine.ps1 ""$chocolateyAppList"" ""$dismAppList"""
